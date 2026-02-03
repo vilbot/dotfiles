4121f1da7daf880af421a61b81d33ec3dcee9875
Vilgot <vilgotskonton@protonmail.com>
2026-02-03 12:20:12 +0100

    dotfiles

--- nvim/lua/plugins-settings.lua
+++ nvim/lua/plugins-settings.lua
@@ -58,7 +58,10 @@ require("telescope").setup({
 })
 
 require("oil").setup({
-    skip_confirm_for_simple_edits = true
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+        show_hidden = true
+    }
 })
 require("nvim-autopairs").setup({})
 require("tabout").setup({})
