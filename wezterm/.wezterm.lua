d6739f5c0f6c46d99d10029679bb83bb009963b2
vilbot <vilgotskonton@protonmail.com>
2026-02-12 12:06:58 +0100

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,38 +1,5 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
-local mux = wezterm.mux
-
-local is_windows = wezterm.target_triple:find("window") ~= nil
-
-local function split_current(direction)
-    return wezterm.action_callback(function(window, pane)
-        local proc = pane:get_foreground_process_name()
-        
-        local my_args = is_windows and { "pwsh.exe", "-NoLogo" } or { "/usr/bin/fish", "-li" }
-        
-        if is_windows and proc then
-            proc_name = proc:lower()
-            -- Check for CMD
-            if proc_name:find("cmd.exe") then
-                my_args = { "cmd.exe" }
-                -- Check for PowerShell
-            elseif proc_name:find("pwsh") or proc_name:find("powershell") then
-                my_args = { "pwsh.exe", "-NoLogo" }
-            end
-        end
-
-        pane:split({
-            direction = direction,
-            args = my_args,
-            cwd = pane:get_current_working_dir() -- optional: keeps you in the same folder
-        })
-    end)
-end
-
--- wezterm.on('gui-startup', function(cmd)
---     local tab, pane, window = mux.spawn_window(cmd or {})
---     window:gui_window():maximize()
--- end)
 
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
   local zoomed = ''
@@ -48,17 +15,6 @@ wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
   return zoomed .. index .. tab.active_pane.title
 end)
 
-local windows_launch_menu = {
-    {
-        label = "PowerShell",
-        args = { "pwsh.exe", "-NoLogo" },
-    },
-    {
-        label = "cmd",
-        args = { "cmd.exe" },
-    },
-}
-
 local my_keys = {
     { mods = 'CTRL|SHIFT', key = 'T', action = act.ShowLauncher },
     { mods = "LEADER", key = "h", action = act.ActivatePaneDirection "Left" },
@@ -68,27 +24,16 @@ local my_keys = {
     { mods = "LEADER", key = "w", action = act.CloseCurrentPane { confirm = true } },
 }
 
-if is_windows then
-    -- Windows: Use the original function that worked
-    table.insert(my_keys, { mods = "LEADER", key = "v", action = split_current("Right") })
-    table.insert(my_keys, { mods = "LEADER", key = "s", action = split_current("Bottom") })
-else
-    -- Linux: Standard native splits (prevents the errors you saw)
-    table.insert(my_keys, { mods = "LEADER", key = "v", action = act.SplitPane { direction = "Right", command = { domain = "CurrentPaneDomain" } } })
-    table.insert(my_keys, { mods = "LEADER", key = "s", action = act.SplitPane { direction = "Down", command = { domain = "CurrentPaneDomain" } } })
-end
-
 return {
-    default_prog = is_windows and { "pwsh.exe", "-NoLogo" } or { '/usr/bin/fish', '-li'},
-    launch_menu = is_windows and windows_launch_menu or {},
+    default_prog = { '/usr/bin/fish', '-li'},
 
     -- APPEARANCE
     font = wezterm.font {
-        -- family = 'Noto Sans Mono',
+        -- family = 'Liberation Mono',
         family = 'DroidSansMonoSlashed',
+        -- family = 'Noto Sans Mono',
         -- family = 'IBM Plex Mono',
         -- family = 'JetBrains Mono',
-        -- family = 'Liberation Mono',
         -- family = 'Ubuntu Mono',
     },
 
