1b75e940824c894d31043227c3c8e535d198067a
vilbot <vilgotskonton@protonmail.com>
2026-02-12 12:27:22 +0100

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -15,19 +15,14 @@ wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
   return zoomed .. index .. tab.active_pane.title
 end)
 
-local my_keys = {
-    { mods = 'CTRL|SHIFT', key = 'T', action = act.ShowLauncher },
-    { mods = "LEADER", key = "h", action = act.ActivatePaneDirection "Left" },
-    { mods = "LEADER", key = "l", action = act.ActivatePaneDirection "Right" },
-    { mods = "LEADER", key = "k", action = act.ActivatePaneDirection "Up" },
-    { mods = "LEADER", key = "j", action = act.ActivatePaneDirection "Down" },
-    { mods = "LEADER", key = "w", action = act.CloseCurrentPane { confirm = true } },
-}
-
 return {
     default_prog = { '/usr/bin/fish', '-li'},
 
-    -- APPEARANCE
+    -- Colorscheme
+    color_scheme = 'Gruber (base16)',
+    -- color_scheme = 'Vesper',
+
+    -- Fonts
     font = wezterm.font {
         -- family = 'Liberation Mono',
         family = 'DroidSansMonoSlashed',
@@ -36,27 +31,35 @@ return {
         -- family = 'JetBrains Mono',
         -- family = 'Ubuntu Mono',
     },
-
     font_size = 11.0,
     adjust_window_size_when_changing_font_size = false,
 
-    color_scheme = 'Gruber (base16)',
-    -- color_scheme = 'Vesper',
-
-    enable_tab_bar = false,
-    window_decorations = "RESIZE|TITLE",
-
-    window_frame = {
-        active_titlebar_bg = '#000000',
-        inactive_titlebar_bg = '#000000',
-    },
-
+    -- Window and tabs
     window_padding = { left = 5, right = 5, top = 10, bottom = 0 },
     initial_rows = 40,
     initial_cols = 120,
+    window_decorations = "RESIZE|TITLE",
 
-    -- KEY BINDINGS
+    enable_tab_bar = true,
+    use_fancy_tab_bar = true,
+    show_close_tab_button_in_tabs = false,
+    show_new_tab_button_in_tab_bar = false,
+
+    window_frame = {
+        active_titlebar_bg = '#181818',
+        inactive_titlebar_bg = '#181818',
+    },
+
+    -- Keybinds
     leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 },
 
-    keys = my_keys,
+    keys = {
+        { mods = "LEADER", key = "h", action = act.ActivatePaneDirection "Left" },
+        { mods = "LEADER", key = "l", action = act.ActivatePaneDirection "Right" },
+        { mods = "LEADER", key = "k", action = act.ActivatePaneDirection "Up" },
+        { mods = "LEADER", key = "j", action = act.ActivatePaneDirection "Down" },
+        { mods = "LEADER", key = "w", action = act.CloseCurrentPane { confirm = true } },
+        { mods = "LEADER", key = "v", action = act.SplitPane { direction = "Right" } },
+        { mods = "LEADER", key = "s", action = act.SplitPane { direction = "Down" } },
+    },
 }
